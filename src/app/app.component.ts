3f0554ff2ac3a1eb08d35c03e8182fda59b00196
Masih Sadat <masih.sadat@vergeag.com>
2022-06-06 11:53:20 -0600

    fifth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'verge2_angular_app';
+  title = 'verge22_angular_app';
 }
